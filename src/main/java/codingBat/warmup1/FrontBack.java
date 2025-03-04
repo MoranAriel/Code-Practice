765e4f693d71d04baea940b6734bc38e50b70198
Moran Ariel <141998962+MoranAriel@users.noreply.github.com>
2025-03-04 15:20:54 +0200

    Update FrontBack Class

--- src/main/java/codingBat/warmup1/FrontBack.java
+++ src/main/java/codingBat/warmup1/FrontBack.java
@@ -4,11 +4,12 @@ public class FrontBack {
 
     //  Given a string, return a new string where the first and last chars have been exchanged.
     public static void main(String[] args) {
-        String str = "";
-        System.out.println(frontBack(str));
+        String str = "moran";
+        System.out.println(frontBack1(str));
+        System.out.println(frontBack2(str));
     }
 
-    public static String frontBack(String str) {
+    public static String frontBack1(String str) {
         if (str.length()>1) {
             String newStr = str.substring(str.length() - 1);
             newStr += str.substring(1, str.length() - 1);
@@ -17,6 +18,15 @@ public class FrontBack {
         } else {
             return str;
         }
+    }
+
+    public static String frontBack2(String str) {
+        if (str.length() <= 1) return str;
+
+        String mid = str.substring(1, str.length()-1);
+
+        // last + mid + first
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
     }
 
 }
